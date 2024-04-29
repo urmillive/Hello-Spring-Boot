7d2a84f971981cc30dd55a7d262b75148786608c
urmil404 <urmil8989@gmail.com>
2024-04-29 22:55:22 +0530

    Added initial configurations

---
+++ src/main/java/com/first/first/FreshApplication.java
@@ -0,0 +1,13 @@
+package com.first.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/first/FreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
